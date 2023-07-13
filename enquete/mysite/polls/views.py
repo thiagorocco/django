87be04eb6fce63f84ea5f903fde40617d36f348c
thiagorocco <deroccotr@gmail.com>
2023-07-12 21:34:45 -0300

    atualização de site

--- enquete/mysite/polls/views.py
+++ enquete/mysite/polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Olá, esse é o meu primeiro site")
+    return HttpResponse("Olá, esse é o meu primeiro site com django")
