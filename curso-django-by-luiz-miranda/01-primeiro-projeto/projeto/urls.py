0de86bc6e9819862109b8093851ebfb454246a3f
thiago-rocco <deroccotr@gmail.com>
2023-07-27 07:06:07 -0300

    linkando a novo arquivo urls.py ao urls.py do projeto e configurando a view

--- curso-django-by-luiz-miranda/01-primeiro-projeto/projeto/urls.py
+++ curso-django-by-luiz-miranda/01-primeiro-projeto/projeto/urls.py
@@ -16,12 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import path
-
-def myView(request):
-    return HttpResponse('Olá, mundo')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/',myView)
+    path('',include('recipes.urls'))
 ]
