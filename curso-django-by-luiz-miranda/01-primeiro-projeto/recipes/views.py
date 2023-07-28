b81701606a167395a2c0e0931832e61ee96fec30
thiago-rocco <deroccotr@gmail.com>
2023-07-28 05:51:46 -0300

    Criação de templates

--- curso-django-by-luiz-miranda/01-primeiro-projeto/recipes/views.py
+++ curso-django-by-luiz-miranda/01-primeiro-projeto/recipes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>HOME</h1>')
+    return render(request,'home.html')
 
 def contato(request):
     return HttpResponse('<h1>CONTATO</h1>')
