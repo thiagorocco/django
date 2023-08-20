eb142525d4210b7a154b3441dbd8f8d64b553791
thiago-rocco <deroccotr@gmail.com>
2023-08-20 09:37:38 -0300

    Adicionando name na urls_patterns do django

--- curso-django-by-luiz-miranda/01-primeiro-projeto/recipes/urls.py
+++ curso-django-by-luiz-miranda/01-primeiro-projeto/recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-#from recipes.views import home
-#from recipes.views import *
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path('recipes/<int:id>/',views.recipe),
+    path('',views.home,name="home"),
+    path('recipes/<int:id>/',views.recipe,name="recipe"),
 ]
