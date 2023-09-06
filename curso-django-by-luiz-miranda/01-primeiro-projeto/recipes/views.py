4486aab327806cb4eb937f4e8b46417a75745e0d
thiago-rocco <deroccotr@gmail.com>
2023-09-06 05:44:52 -0300

    Criação de novo projeto django para aprendizado

--- curso-django-by-luiz-miranda/01-primeiro-projeto/recipes/views.py
+++ curso-django-by-luiz-miranda/01-primeiro-projeto/recipes/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
-
 # Create your views here.
 def home(request):
     return render(request,'recipes/pages/home.html',context={
