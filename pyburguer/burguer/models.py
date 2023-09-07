c90dfdae64e680b93d801e2a8d6ca601b225d0fa
thiago-rocco <deroccotr@gmail.com>
2023-09-07 14:12:56 -0300

    Criação da Model Produto, criação de migrations dos apss e manipulação de registros na tabela Produto

--- pyburguer/burguer/models.py
+++ pyburguer/burguer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    descricao = models.TextField(null=False, blank=False)
+    preco = models.FloatField(null=False, blank=False)
+
+    def __str__(self):
+        return self.descricao
