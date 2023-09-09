64cf37f6ff6d1faa6300f26f63a06c23ada822dd
thiago-rocco <deroccotr@gmail.com>
2023-09-09 08:33:14 -0300

    Inserindo imagens estáticas no projeto, constantes de settings.py configuradas

--- pyburguer/burguer/models.py
+++ pyburguer/burguer/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Produto(models.Model):
     descricao = models.TextField(null=False, blank=False)
     preco = models.FloatField(null=False, blank=False)
+    imagem = models.ImageField(upload_to='burguer/img', default='', null=False, blank=False)
 
     def __str__(self):
         return self.descricao
