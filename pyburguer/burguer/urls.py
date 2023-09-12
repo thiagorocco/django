95989e86db6865e5147fe5787c9b4767ab325ad3
thiago-rocco <deroccotr@gmail.com>
2023-09-12 06:21:35 -0300

    Finalizado elementos da navbar e criação do template banner com carregamento de imagens estáticas

--- pyburguer/burguer/urls.py
+++ pyburguer/burguer/urls.py
@@ -7,4 +7,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('produto', views.produto, name='produto'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
