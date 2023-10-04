4b0224adf7528f2f087d254a5abd2e02f905d208
thiago-rocco <deroccotr@gmail.com>
2023-10-03 21:37:55 -0300

    Criação da classe Produto em models.py

--- pycakes/cakes/views.py
+++ pycakes/cakes/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello, World!!!')
+    return render(request, 'cakes/home.html')
