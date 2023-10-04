c3fdfa6ef4eba31e0e2e1da54964366e9df89bce
thiago-rocco <deroccotr@gmail.com>
2023-10-04 06:58:36 -0300

    criação da pasta img em cakes

--- pycakes/cakes/models.py
+++ pycakes/cakes/models.py
@@ -5,4 +5,4 @@ class Produto(models.Model):
     nome = models.TextField(null=False, blank=False, default="")
     descricao = models.TextField(null=False, blank=False)
     preco = models.FloatField(null=False, blank=False)
-    imagem = models.ImageField(upload_to='burguer/img', default='', null=False, blank=False)
+    imagem = models.ImageField(upload_to='cakes/img', default='', null=False, blank=False)
