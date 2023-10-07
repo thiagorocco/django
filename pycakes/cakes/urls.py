647ed9c7a88a951d820d4722c2d54f9adb3a27cb
thiago-rocco <deroccotr@gmail.com>
2023-10-06 21:52:10 -0300

    Formatação de imagens do detalhe do produto concluída. Projeto pycakes concluído

--- pycakes/cakes/urls.py
+++ pycakes/cakes/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('produtos/<int:produto_id>', views.detalhe_produto, name='produtos'),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
