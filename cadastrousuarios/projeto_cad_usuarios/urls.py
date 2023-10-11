56937efe29fd0cf8e5d8486fb425fc7c58eae50d
thiago-rocco <deroccotr@gmail.com>
2023-10-11 06:53:48 -0300

    Criada uma rota para a url raiz('') com a view home

--- cadastrousuarios/projeto_cad_usuarios/urls.py
+++ cadastrousuarios/projeto_cad_usuarios/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_cad_usuarios import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+ 
 ]
