a48a04e79ce55ff75ad921851b47335f0f666b5d
thiago-rocco <deroccotr@gmail.com>
2023-10-14 10:00:21 -0300

    view home alterada para retornar os dados da Model Pessoa em index.html

--- crud/app/views.py
+++ crud/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from app.models import Pessoa
 
-# Create your views here.
 def home(request):
-    return render(request, 'app/index.html')
+    pessoas = Pessoa.objects.all()
+
+    #Isso dará acesso aos dados do banco de dados no template index.html
+    return render(request,'app/index.html',{"pessoas": pessoas})
