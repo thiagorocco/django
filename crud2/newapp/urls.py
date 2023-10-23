6921f2a9cf0a730943f72807767f3180c74cca01
thiago-rocco <deroccotr@gmail.com>
2023-10-23 07:05:30 -0300

    view delete configurada e funcionando perfeitamente

--- crud2/newapp/urls.py
+++ crud2/newapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add/',views.add, name='add' ),
     path('addrec/',views.addrec, name='addrec'),
-    path('delete/',views.delete, name='delete'),
+    path('delete/<int:id>/',views.delete, name='delete')
 ]
