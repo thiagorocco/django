09e00a017ea306e90f9eb6f52319920c0746eed2
thiago-rocco <deroccotr@gmail.com>
2023-10-24 06:54:22 -0300

    Criação da view uprec

--- crud2/newapp/views.py
+++ crud2/newapp/views.py
@@ -24,3 +24,14 @@ def delete(request, id):
 def update(request, id):
     mem = Member.objects.get(id=id)
     return render(request,'update.html',{'mem':mem})
+
+def uprec(request, id):
+    x = request.POST['first']
+    y = request.POST['last']
+    z = request.POST['country']
+    mem = Member.objects.get(id=id)
+    mem.firstname = x
+    mem.lastname = y
+    mem.country = z
+    mem.save()
+    return redirect('/') 
