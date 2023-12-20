d790ab7cae267a38066a8994f911ee9aa8d6f2ca
thiagorocco <deroccotr@gmail.com>
2023-12-20 06:52:55 -0300

    pastas movidas para outro diretório

--- curso-django-by-luiz-miranda/01-primeiro-projeto/recipes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.home,name="recipes-home"),
-    path('recipes/<int:id>/',views.recipe,name="recipes-recipe"),
-]
