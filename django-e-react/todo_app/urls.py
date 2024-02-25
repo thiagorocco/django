2bfa7f14eecae4347cb5b7145fc9cae63d00d985
thiagorocco <deroccotr@gmail.com>
2024-02-25 16:25:18 -0300

    Criação do template index.html

---
+++ django-e-react/todo_app/urls.py
@@ -0,0 +1,8 @@
+# todo_app/urls.py
+from django.urls import path
+from .views import TaskList, TaskDetail
+
+urlpatterns = [
+    path('tasks/', TaskList.as_view(), name='task-list'),
+    path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+]
